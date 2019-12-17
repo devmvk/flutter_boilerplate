ca90bd51e6fb0f3799f5c3785d25e44d20756fd3
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-12-17 18:14:53 +0530

    add app data interface to app config

--- lib/appconfig.dart
+++ lib/appconfig.dart
@@ -1,8 +1,13 @@
 
 
+import 'package:project_boilerplate/services/app_data_interface.dart';
 import 'package:project_boilerplate/tools/fcm_component.dart';
 
-class AppConfig{
+class AppConfig{ 
+
+  final AppDataInterface _appDataInterface = AppDataInterface.instance;
+  AppDataInterface get appDataInterface => this._appDataInterface;
+
   AppConfig(){
     FCMComponent.instance.init();
   }
