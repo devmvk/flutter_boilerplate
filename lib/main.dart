fbed8bfd6ed7882edf20cb749f061caa974617d0
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-12-17 17:39:03 +0530

    format

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:project_boilerplate/appconfig.dart';
 import 'package:provider/provider.dart';
- 
+
 void main() => runApp(Devmvk());
- 
+
 class Devmvk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Provider<AppConfig>(
       create: (BuildContext context) => AppConfig(),
-          child: MaterialApp(
+      child: MaterialApp(
         title: 'Material App',
         home: Scaffold(
           appBar: AppBar(
