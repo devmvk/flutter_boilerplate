a2a5ab559c1550c45a4c32fe7d0852159ebb1a8b
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-12-24 20:16:12 +0530

    implement  google and facebook authentication

--- lib/blocs/authentication_bloc.dart
+++ lib/blocs/authentication_bloc.dart
@@ -1,4 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:project_boilerplate/services/handlers/firebase/firebase_authentication_handler.dart';
 
 class AuthenticationBloc{
+  final AuthBase auth;
+  final ValueNotifier<bool> isLoading;
+  AuthenticationBloc({@required this.auth, @required this.isLoading});
   
+  
+  Future<User> _signIn(Future<User> Function() signInMethod) async{
+    try{
+      isLoading.value = true;
+      return await signInMethod();
+    }catch(e){
+      isLoading.value = false;
+      rethrow;
+    }
+  }
+
+  Future<User> signInAnonymously() async => await _signIn(auth.signInAnonymously);
+  Future<User> googleSignIn() async => await _signIn(auth.googleSignIn);
+  Future<User> facebookSignIn() async => await _signIn(auth.facebookSignIn);
 }
---
+++ lib/services/handlers/firebase/firebase_authentication_handler.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+
+abstract class AuthBase{
+
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<User> googleSignIn();
+  Future<User> facebookSignIn();
+  Future<User> emailAndPasswordSignIn(String email, String password);
+  Future<User> createAccount(String email, String password);
+  Future<void> signOut();
+  Stream<User> get onAuthStateChanged;
+
+}
+
+class User{
+  final String uid;
+  final String displayName;
+  final String avatarUrl;
+  User({@required this.uid, @required this.avatarUrl, @required this.displayName});
+}
