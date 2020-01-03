110a4de9b401dc62cfe3a18e196fc07549f6115b
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2020-01-03 22:53:25 +0530

    Revert "Revert " implement google, facebook, email, phone and anonymous authentication""

---
+++ lib/services/firebase_authentication_handler.dart
@@ -0,0 +1,22 @@
+import 'package:meta/meta.dart';
+
+abstract class AuthBase{
+
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<User> googleSignIn();
+  Future<User> facebookSignIn();
+  Future<User> emailAndPasswordSignIn(String email, String password);
+  Future<User> createAccount(String email, String password);
+  Future<void> signOut();
+  Future<User> mobileSignIn();
+  Stream<User> get onAuthStateChanged;
+  Future<void> verifyPhoneNumber({@required String mobileNumber});
+}
+
+class User{
+  final String uid;
+  final String displayName;
+  final String avatarUrl;
+  User({@required this.uid, @required this.avatarUrl, @required this.displayName});
+}
